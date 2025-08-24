fef3d0a23469c820deede4a9232739ac9c63fc70
romanzavarzin <romalbj@mail.ru>
2025-08-24 12:38:32 +0300

    statistics page + smooth snap scroll + js timer on appear (pc n mobile version)

--- juridical_site/main/static/main/js/script.js
+++ juridical_site/main/static/main/js/script.js
@@ -26,6 +26,88 @@ navLinks.forEach (link => {
 //        console.log(link);
 })
 
+//доделать
+const statisticsBlock = document.querySelector('.statistics')
+const logo = document.querySelector('.logo')
+const aboutUs = document.querySelector('.statistics .statistics_wrapper .heading p')
+const statistics = document.querySelector('.statistics .statistics_wrapper ul')
+const statisticsOne = document.getElementById('1')
+const statisticsTwo = document.getElementById('2')
+const statisticsThree = document.getElementById('3')
+const statisticsFour = document.getElementById('4')
+
+//const header = document.querySelector('.nav_name')
 
 
+window.addEventListener('scroll', () => {
+    if (pageYOffset > window.innerHeight) {
+        statisticsBlock.style.backgroundAttachment = 'local';
+    }
 
+    else if (pageYOffset > window.innerHeight*0.5) {
+        const timerIdZero = setTimeout(() => {
+          aboutUs.style.opacity = '1';
+        }, 300);
+
+        const timerIdOne = setTimeout(() => {
+          statisticsOne.style.opacity = '1';
+        }, 675);
+
+        const timerIdTwo = setTimeout(() => {
+          statisticsTwo.style.opacity = '1';
+        }, 800);
+
+        const timerIdThree = setTimeout(() => {
+          statisticsThree.style.opacity = '1';
+        }, 925);
+
+        const timerIdFour = setTimeout(() => {
+          statisticsFour.style.opacity = '1';
+        }, 1050);
+
+
+        aboutUs.style.transition = '3s ';
+        statisticsOne.style.transition = '2s';
+        statisticsTwo.style.transition = '2s';
+        statisticsThree.style.transition = '2s';
+        statisticsFour.style.transition = '2s';
+
+
+        logo.addEventListener('mouseover', function(e) {
+            if (logo) logo.style.boxShadow = '0px 5px 25px 0.7px rgba(171, 63, 44, 0.6)';
+        });
+          logo.addEventListener('mouseout', function(e) {
+            if (logo) {
+                logo.style.boxShadow = '0px 0px 0px 0px transparent';
+            }
+        });
+        }
+
+    else if (pageYOffset < window.innerHeight*0.5) {
+        aboutUs.style.opacity = '0';
+        statisticsOne.style.opacity = '0';
+        statisticsTwo.style.opacity = '0';
+        statisticsThree.style.opacity = '0';
+        statisticsFour.style.opacity = '0';
+
+        aboutUs.style.transition = '.1s';
+        statisticsOne.style.transition = '.1s';
+        statisticsTwo.style.transition = '.1s';
+        statisticsThree.style.transition = '.1s';
+        statisticsFour.style.transition = '.1s';
+
+
+        logo.addEventListener('mouseover', function(e) {
+            if (logo) logo.style.boxShadow = '0px 5px 25px 0.7px rgba(96, 33, 33, 0.5)';
+        });
+          logo.addEventListener('mouseout', function(e) {
+            if (logo) {
+                logo.style.boxShadow = '0px 0px 0px 0px transparent';
+            }
+        });
+        }
+
+    else if (pageYOffset < window.innerHeight) {
+        statisticsBlock.style.backgroundAttachment = 'fixed';
+    }
+});
